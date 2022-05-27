6ab2ddd7db1ba7d10bfca5d61365adab6e1c6412
zongzuo <542676491@qq.com>
2022-05-27 17:06:56 +0800

    同步下代码

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-let a: number = 123;
-
-const h1 = document.createElement("h1");
-h1.innerHTML = "Hello, I am Lison";
-document.body.appendChild(h1);
+let list1: number[] = [1, 2, 3];
+let list2: Array<number> = [1, 2, 3];
